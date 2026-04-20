f326d0db865220ceca1fd8bb1516fcb42e232155
Caio Vilquer Carvalho <caio@vilquer.dev>
2026-04-19 21:04:10 -0300

    feat: inicializando projeto Spring Boot com Maven

---
+++ src/main/java/br/usp/lab/oo/planejador_feriado/PlanejadorFeriadoApplication.java
@@ -0,0 +1,13 @@
+package br.usp.lab.oo.planejador_feriado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejadorFeriadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejadorFeriadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/usp/lab/oo/planejador_feriado/PlanejadorFeriadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usp.lab.oo.planejador_feriado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejadorFeriadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
